f42bac412ad343a5916f451a956cad1a7896efbb
gh-yanapon <i.yanapon@live.jp>
2020-02-16 11:53:41 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-echo 'Hello php';
+<?
+echo 'phpのテスト';
+
+$dbinfo = parse_url(getenv('DATABASE_URL'));
+
+$dsn = 'pgsql:host=' . $dbinfo['ec2-184-72-236-57.compute-1.amazonaws.com'] . ';dbname=' . substr($dbinfo['path'],1);
+
+$pdo = new PDO($dsn, $dbinfo['sbzqiggebhgruh'], $dsinfo['5f93db4c254634fa20ce3cec96f5caebe4df96186f4c196af214049d4aa6c446']);
+var_dump($pdo-->getattribute(PDO::ATTR_SERVER_VERSION));
+
+phpinfo();
 ?>
